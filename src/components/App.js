0b9fc39aff06c0e3808fcc3582da759278824274
Kaumil Manzoor <kaumilmanzoor@gmail.com>
2020-01-21 13:39:53 -0500

    Set up Header link tags to Team and Games pages using React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import PlayersList from './PlayersList';
 import PlayerDetails from './PlayerDetails';
 import '../App.css';
+import Header from './Header';
+import TeamPage from './TeamPage';
+import GamePage from './GamePage';
+
 
 const App = () => {
   return (
     <div className="App">
-          <h1>Raptors App</h1>
-          <h3>Testing new branch</h3>
-          <div id="container">
-            <PlayersList />
-            <PlayerDetails />
-          </div>
+      <BrowserRouter >
+        <Header/>
+        <div id="container">
+            <Route path="/" exact component={PlayersList}/>
+            <Route path="/" exact component={PlayerDetails} />
+            <Route path="/teams" component={TeamPage} />
+            <Route path="/games" component={GamePage} />
+        </div>
+      </BrowserRouter>
+          
     </div>
   );
 }
