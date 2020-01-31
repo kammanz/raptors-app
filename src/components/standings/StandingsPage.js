ac327bfeda23f40f784609068a22ce2bac2f5a25
Kaumil Manzoor <kaumilmanzoor@gmail.com>
2020-01-31 14:12:15 -0500

    minor fixes

--- src/components/standings/StandingsPage.js
+++ src/components/standings/StandingsPage.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const StandingsPage = () => {
-<<<<<<< HEAD
-    return <div>This is the StandingsPage</div>;
-=======
     return <div>StandingsPage</div>;
->>>>>>> RAP-22b-Should-route-to-playersPage
 }
 
 export default StandingsPage;
