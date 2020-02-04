942fad4d71bf0a219e5085f209837517e23ec00c
Kaumil Manzoor <kaumilmanzoor@gmail.com>
2020-02-04 15:08:52 -0500

    added abadi font

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <ul className="nav-bar">
-            <li>Raptors logo</li>
+            <li className="header team-name">Toronto Raptors</li>
             <Link to='/'>Players</Link>
             <Link to='/standings'>Standings</Link>
             <Link to='/games'>Games</Link>
