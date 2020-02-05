4e90c1419a401f68b704414ddd0444eb418388e5
Kaumil Manzoor <kaumilmanzoor@gmail.com>
2020-02-05 15:10:18 -0500

    [#21] finished styling user section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import raptorsLogo from '../assets/raptors-logo-2.png';
-import bell from '../assets/bell3.png';
-import settingsIcon from '../assets/settings-icon3.png';
+import bell from '../assets/bell7.png';
+import settingsIcon from '../assets/settings-icon7.png';
 import kobe from '../assets/kobe.jpg';
 
 const Header = () => {
@@ -32,13 +32,13 @@ const Header = () => {
             
             <div className="header user">
                 <div>
-                    <img src={bell} alt="notification-bell" height="20"></img>
+                    <img src={bell} alt="notification-bell" height="25"></img>
                 </div>
                 <div>
-                    <img src={settingsIcon} alt="settings-icon" height="20"></img>
+                    <img src={settingsIcon} alt="settings-icon" height="25"></img>
                 </div>
-                <div>
-                    <img src={kobe} alt="user-pic" height="20"></img>
+                <div >
+                    <img src={kobe} alt="user-pic" height="30" className="user-pic"></img>
                 </div>
                 <div>Kobe Bryant</div>
                 <div className="drop-down user"></div>
