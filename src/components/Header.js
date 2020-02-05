cc1df4df935452271ca146079a577daaaecf9587
Kaumil Manzoor <kaumilmanzoor@gmail.com>
2020-02-05 14:30:33 -0500

    [#21] completed styling of header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import raptorsLogo from '../assets/raptors-logo-2.png';
+import bell from '../assets/bell3.png';
+import settingsIcon from '../assets/settings-icon3.png';
+import kobe from '../assets/kobe.jpg';
 
 const Header = () => {
     return (
         <div className="header container">
             <div className="header team">
                 <div className="logo-container">
-                    <img src={raptorsLogo} height="50" title="raptors logo" alt="raptors logo"></img>
+                    <img src={raptorsLogo} height="40" title="raptors logo" alt="raptors logo"></img>
                 </div>
                 <div>Toronto Raptors</div>
-                <div>Dropdown</div>
+                <div className="drop-down">
+                    <i className="down"></i>
+                </div>
             </div>
-            {/* <div className=" header slanted-border"></div> */}
+
             <div className="header links">
-                <Link to='/'>Players</Link>
-                <Link to='/standings'>Standings</Link>
-                <Link to='/games'>Games</Link>
+                <div className="link-container">
+                    <Link to='/'>Players</Link>
+                </div>
+                <div className="link-container">
+                    <Link to='/standings'>Standings</Link>
+                </div>
+                <div className="link-container">
+                    <Link to='/games'>Games</Link>
+                </div>
             </div>
             
-            <div className="header user">username</div>
+            <div className="header user">
+                <div>
+                    <img src={bell} alt="notification-bell" height="20"></img>
+                </div>
+                <div>
+                    <img src={settingsIcon} alt="settings-icon" height="20"></img>
+                </div>
+                <div>
+                    <img src={kobe} alt="user-pic" height="20"></img>
+                </div>
+                <div>Kobe Bryant</div>
+                <div className="drop-down user"></div>
+            </div>
         </div>
     );
 }
