d8dde8419856d34a4750024dd85dc44914673181
kermit <kermy19800@gmail.com>
2020-03-20 10:22:59 -0400

    [#36] Refactored quickStatContainer

--- src/components/players/playerDetails/stats.js
+++ src/components/players/playerDetails/stats.js
@@ -4,8 +4,8 @@ import styles from './stats.module.scss';
 
 const Stats = (props) => {
     return (
-        <div className={styles.container}>
-            <div className={props.isQuickStats ? styles.quickStatTitle : styles.title }>{props.title}</div>
+        <div className={props.isQuickStats ? styles.quickStatContainer : styles.container}>
+            <div className={props.isQuickStats ? styles.quickStatTitle : styles.title}>{props.title}</div>
             <div className={props.isQuickStats ? styles.quickStat : styles.stat}>{props.stat}</div>
         </div>
     )
