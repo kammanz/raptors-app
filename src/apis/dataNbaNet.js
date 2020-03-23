854cfb3c0b604893c13f5d3b34bb222b440f29b5
kermit <kermy19800@gmail.com>
2020-03-23 13:27:02 -0400

    [#37] Reverted the api get request url back to heroku, as issue was reslolved

--- src/apis/dataNbaNet.js
+++ src/apis/dataNbaNet.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-     // baseURL: 'https://cors-anywhere.herokuapp.com/http://data.nba.net',
-     baseURL: 'http://data.nba.net',
+     baseURL: 'https://cors-anywhere.herokuapp.com/http://data.nba.net',
+     // baseURL: 'http://data.nba.net',
 })
