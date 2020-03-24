042863cc891552699512ad397cb847644d6e7088
kermit <kermy19800@gmail.com>
2020-03-23 21:08:01 -0400

    [minor fixes]

--- src/components/players/playerDetails/stats.js
+++ src/components/players/playerDetails/stats.js
@@ -4,12 +4,13 @@ import Title from './title.js';
 
 import styles from './stats.module.scss';
 
-const Stats = (props) => {
-    const { isQuickStats, title, stat } = props;
-
+const Stats = ({ isQuickStats, title, stat }) => {
     return (
         <div className={isQuickStats ? styles.quickStatContainer : styles.container}>
-            {isQuickStats ? <Title title={title} /> : <div className={styles.title}>{title}</div>}
+            {isQuickStats ? 
+                <Title title={title} /> : 
+                <div className={styles.title}>{title}</div>
+            }
             <div className={styles.stat}>{stat}</div>
         </div>
     )
