9d44c452153b814c9ae8e72fff5afa3804fe1e63
Tumne <freshlygrazed@gmail.com>
2020-03-25 01:24:19 -0400

    [#37] Simplify code

--- src/components/players/playerDetails/gameResult.js
+++ src/components/players/playerDetails/gameResult.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import styles from './gameResult.module.scss';
 import Stats from './stats';
 
-const GameResult = ({teams, games}) => {
+const GameResult = ({ teams, games }) => {
     const gamesArray = Object.values(games);
-    // console.log(gamesArray, 'cunt');
     const teamsArray = Object.values(teams);
+    console.log(gamesArray, teamsArray);
 
     const opposingTeamArray = gamesArray.map((game)=> {
-
-        console.log(game.stats, 'pickle');
-
-        if (game.isHomeGame === false) {
-            return { ...game.hTeam, gameDate: game.gameDateUTC, isHomeGame: game.isHomeGame, selectedTeamScore: game.vTeam.score, playerGameStats: game.stats };
-        } else {
-            return { ...game.vTeam, gameDate: game.gameDateUTC, selectedTeamScore: game.hTeam.score, playerGameStats: game.stats };
-        }
+        return {
+          ...(game.isHomeGame ? game.vTeam : game.hTeam),
+          gameDate: game.gameDateUTC,
+          isHomeGame: game.isHomeGame,
+          selectedTeamScore: game.isHomeGame ? game.hTeam.score : game.vTeam.score,
+          playerGameStats: game.stats
+        };
     });
 
     const threeMatchingTeams = (opposingTeamArray, teamsArray) => {
@@ -37,17 +35,16 @@ const GameResult = ({teams, games}) => {
     const matchingTeamArray = threeMatchingTeams(opposingTeamArray, teamsArray);
 
     const individualGame = matchingTeamArray.map((game, index)=>{
-        console.log(game, 'here yo');
         const { isHomeGame, tricode, ttsName, gameDate, score, isWinner, selectedTeamScore, playerGameStats } = game;
         const { points, offReb, defReb, totReb, assists } = playerGameStats;
         // console.log(points, 'fuck up');
         const parsedDate = new Date(gameDate).toDateString();
         // const parsedDate2 = parsedDate.toDateString();
         return (
-            <div className={styles.container}>
+            <div key={index} className={styles.container}>
                 <div className={styles.titleContainer}>
-                    {isHomeGame ? 
-                        <div key={index}>vs</div> : 
+                    {isHomeGame ?
+                        <div key={index}>vs</div> :
                         <div key={index}>@</div>
                     }
                     <img src={`https://cdn.nba.net/assets/logos/teams/secondary/web/${tricode}.svg`} height="50" />
@@ -55,10 +52,10 @@ const GameResult = ({teams, games}) => {
                     <div>{parsedDate}</div>
                 </div>
                 <div className={styles.resultsContainer}>
-                    {isWinner ? 
+                    {isWinner ?
                         <div>L</div> : <div>W</div>
                     }
-                    {isWinner ? 
+                    {isWinner ?
                         <div>{score} - {selectedTeamScore}</div> :
                         <div>{selectedTeamScore} - {score}</div>
                     }
@@ -70,10 +67,10 @@ const GameResult = ({teams, games}) => {
                         <div>total rebounds: {totReb}</div>
                         <div>assists: {assists}</div> */}
                     </div>
-                    
-                    <Stats title="Points" stats={points} />
+
+                    <Stats title="Points" stat={points} />
                 </div>
-                
+
             </div>
         )
     })
@@ -81,20 +78,41 @@ const GameResult = ({teams, games}) => {
     // console.log(individualGame);
 
     return (
-        <div>{individualGame}</div>
+        // <div>{individualGame}</div>
+        <div>
+          {
+            gamesArray.map(({
+              gameId,
+              isHomeGame,
+              hTeam,
+              vTeam,
+            }) => {
+              const homeTeam = isHomeGame ? hTeam : vTeam;
+              const oppTeamId = isHomeGame ? vTeam.teamId : hTeam.teamId;
+              const oppTeamObj = teamsArray.find(team => team.teamId === oppTeamId);
+
+              const gameResult = homeTeam.isWinner ? 'W' : 'L';
+              const score = parseInt(hTeam.score) > parseInt(vTeam.score) ? `${hTeam.score} - ${vTeam.score}` : `${vTeam.score} - ${hTeam.score}`;
+              return (
+                <div key={gameId}>
+                  <div>{isHomeGame ? 'vs' :'@'}</div>
+                  <div>{oppTeamId}</div>
+                  <div>{oppTeamObj.ttsName} {oppTeamObj.tricode}</div>
+                  <div>{gameResult}</div>
+                  <div>{score}</div>
+                </div>
+              );
+          })}
+        </div>
         // <div>
         //     {isHomeGame ? <div>@</div> : <div>vs</div>}
         //     <div>{opponentName}</div>
         //     <img src={`https://cdn.nba.net/assets/logos/teams/secondary/web/${tricode}.svg`} />
         //     <div>{date2}</div>
         // </div>
-        
+
     );
     // return <div>Nothing yet</div>;
-}
+};
 
-const mapStateToProps = (state) => {
-    return { teams: state.teams, games: state.games };
-}
-
-export default connect(mapStateToProps)(GameResult);
+export default GameResult;
