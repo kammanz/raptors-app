9ea695e0be5a317295967460a35444c34b361ac5
kermit <kermy19800@gmail.com>
2020-03-27 00:18:08 -0400

    [#37] Formatted date, finished styling of header section

--- src/components/players/playerDetails/gameResult.js
+++ src/components/players/playerDetails/gameResult.js
@@ -5,7 +5,7 @@ import Stats from './stats';
 const GameResult = ({ teams, games }) => {
     const gamesArray = Object.values(games);
     const teamsArray = Object.values(teams);
-    console.log(gamesArray);
+
     return (
         <div>
           {
@@ -17,7 +17,6 @@ const GameResult = ({ teams, games }) => {
               gameDateUTC,
             }) => {
 
-              console.log(gameDateUTC);
               const getGameResult = () => {
                 const playersTeam = isHomeGame ? hTeam : vTeam;
                 return playersTeam.isWinner ? 'W' : 'L';
@@ -32,8 +31,14 @@ const GameResult = ({ teams, games }) => {
                 `${hTeam.score} - ${vTeam.score}` :
                 `${vTeam.score} - ${hTeam.score}`;
 
-              const date = new Date(gameDateUTC).toDateString();  
-              // const date = gameDateUTC.toDateString();  
+              const dateArray = new Date(gameDateUTC).toDateString().split(' ');
+              const formattedDate = (array) => {
+                const el = array[2];
+
+                return (el.charAt(0) === "0" ? 
+                  array[0] + "." + " " + array[1] + " " + el.substring(1) + ", " + array[3]:
+                  array[0] + "." + " " + array[1] + " " + el + ", " + array[3]);
+              }; 
 
               return (
                 <div key={gameId}>
@@ -41,7 +46,7 @@ const GameResult = ({ teams, games }) => {
                     <div>{isHomeGame ? 'vs' :'@'}</div>
                     <img src={`https://cdn.nba.net/assets/logos/teams/secondary/web/${oppTeam.tricode}.svg`} height="42" alt="team logo" />
                     <div>{oppTeam.ttsName} </div>
-                    <div className={styles.date}>{date}</div>
+                    <div className={styles.date}>{formattedDate(dateArray)}</div>
                   </header>
                   <section>
                     <div>{getGameResult()}</div>
