695ea1f5e0ab743f3520005b0ce529d1f9fb80d2
kermit <kermy19800@gmail.com>
2020-04-27 09:34:58 -0400

    [#37] minor fix

--- src/apis/dataNbaNet.js
+++ src/apis/dataNbaNet.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
      // NOTE: Sometimes I hit my get request limit with herokuapp and get blocked. So I switch to another baseurl and use the CORS extension to keep coding. I will remove second baseURL at a later date.  
 
-     // baseURL: 'https://cors-anywhere.herokuapp.com/http://data.nba.net',
-     baseURL: 'http://data.nba.net',
+     baseURL: 'https://cors-anywhere.herokuapp.com/http://data.nba.net',
+     // baseURL: 'http://data.nba.net',
 })
