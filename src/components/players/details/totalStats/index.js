81f35d49650c53385bef4c04fc05a2a4c7476404
kermit <kermy19800@gmail.com>
2020-04-29 12:46:16 -0400

    [#37] Styled QuickStats component

--- src/components/players/details/totalStats/index.js
+++ src/components/players/details/totalStats/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Table from '../../../_shared/table';
 import Title from '../../../_shared/title';
-import './index.module.scss';
+import styles from './index.module.scss';
 
 const TotalStats = ({ totalStats }) => {
     return (
-        <section>
+        <section class={styles.totalStats}>
             <Title title="stats"/>
             <Table array={totalStats}/>
         </section>
