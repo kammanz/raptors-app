cdeef57e5e85b53543b00a4cd65417eb5a3b781d
kermit <kermy19800@gmail.com>
2020-05-14 21:43:43 -0400

    [#53] minor fixes

--- src/components/_shared/title.js
+++ src/components/_shared/title.js
@@ -4,7 +4,7 @@ import styles from './title.module.scss';
 
 const Title = ({ title, section, teamColor }) => {
 
-    return <div style={{ backgroundColor: `${teamColor}`}} className={section === "games" ? styles.gameStats : styles.totalStats}>{title}</div>;
+    return <div style={{backgroundColor: `${teamColor}`}} className={section === "games" ? styles.gameStats : styles.totalStats}>{title}</div>;
 }   
 
 export default Title;
