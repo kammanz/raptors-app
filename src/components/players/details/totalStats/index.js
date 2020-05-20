925bcf31a3c1f5054ba0068d2b2a1110795cce71
Tumne <freshlygrazed@gmail.com>
2020-05-20 01:51:43 -0400

    [#75] Minor fixes

--- src/components/players/details/totalStats/index.js
+++ src/components/players/details/totalStats/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import Spinner from 'components/_shared/spinner';
+import Spinner from '../../../_shared/spinner';
 import Table from 'components/_shared/table';
 import Title from 'components/_shared/title'
 
 import styles from './index.module.scss';
 
 const TotalStats = ({ totalStats, teamColor, isLoading }) => {
-
   return (
       <section className={styles.totalStats}>
           <Title title='stats' totalStats teamColor={teamColor} />
