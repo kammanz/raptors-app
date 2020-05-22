78d53edad64a5fa3381582793e16df9efe672ffb
Tumne <freshlygrazed@gmail.com>
2020-05-22 13:26:50 -0400

    [#81] Completed stick header

--- src/components/players/details/card/index.js
+++ src/components/players/details/card/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import placeholderImg from '../../../../assets/imgs/placeholder.png';
 
@@ -18,24 +19,27 @@ const Card = ({ player, playerTeamId, isSticky }) => {
     } = player;
 
     return (
-        <div className={styles.card}>
-            <div className={styles.imageContainer}>
-                <img
-                    src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${playerTeamId}/2019/260x190/${person_id}.png`} alt='player headshot'
-                    onError={e => e.target.src = placeholderImg}
-                />
-            </div>
+        <>
+          <div className={styles.card}>
+              <div className={styles.imageContainer}>
+                  <img
+                      src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${playerTeamId}/2019/260x190/${person_id}.png`} alt='player headshot'
+                      onError={e => e.target.src = placeholderImg}
+                  />
+              </div>
+          </div>
+          <div className={classnames(styles.card, styles.detailsWrapper, isSticky && styles.borderBottom)}>
             <div style={{ borderColor: teamColor }} className={styles.imageLine} />
             <div className={styles.detailsContainer}>
                 <div className={styles.details}>
-                  {isSticky &&
+                  {/*{isSticky &&
                     <div className={styles.thumbContainer}>
                         <img
                             src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${playerTeamId}/2019/260x190/${person_id}.png`} alt='player headshot'
                             onError={e => e.target.src = placeholderImg}
                         />
                     </div>
-                  }
+                  }*/}
                   <div className={styles.info}>
                     <div className={styles.name}>{first_name} {last_name}</div>
                     <div className={styles.position}>{position_full}</div>
@@ -46,7 +50,8 @@ const Card = ({ player, playerTeamId, isSticky }) => {
                     <div style={{ color: teamColor }} className={styles.jerseyNumber}>{jersey_number}</div>
                 </div>
             </div>
-        </div>
+          </div>
+        </>
     );
 };
 
