8eacbd728d676dc67772753201681b9c5ff7f46e
Tumne <freshlygrazed@gmail.com>
2020-05-23 02:02:06 -0400

    [#81] Clean up

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -2,9 +2,9 @@
  * Formats url string for specific static player image
  *
  * @param {string} playerTeamId
- * @param {string} personId
+ * @param {string} playerId
  * @param {string} year
  * @returns {string}
  */
-export const formatPlayerPhotoUrl = (playerTeamId, personId, year = '2019') =>
-  `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${playerTeamId}/${year}/260x190/${personId}.png`
+export const formatPlayerPhotoUrl = (playerTeamId, playerId, year = '2019') =>
+  `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${playerTeamId}/${year}/260x190/${playerId}.png`
