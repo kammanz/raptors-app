361e62ff538c94978477cfb4cb40bc3f00a26406
kermit <kermy19800@gmail.com>
2020-05-27 12:16:11 -0400

    [#74] Installed react-select

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Select from 'react-select';
 
 import { getTeams, getSelectedTeam } from '../../actions/actions.js';
 import { TEAMS, COLORS } from '../../enums';
