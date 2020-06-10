e40290169429b14628f78a2cb5df659cedb8783a
kermit <kermy19800@gmail.com>
2020-06-10 15:51:38 -0400

    [#61] Clean up

--- src/reducers/getSelectedTeamReducer.js
+++ src/reducers/getSelectedTeamReducer.js
@@ -2,8 +2,6 @@ export default (state = {}, action) => {
   switch (action.type) {
     case 'GET_SELECTED_TEAM':
       return action.payload;
-    case 'GET_TEAM_COLOR':
-      return { ...state, teamColor: action.payload };
     default:
       return state;
   }
