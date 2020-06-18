6be9c5a258930b159e23d9505cdf648e011f312f
kermit <kermy19800@gmail.com>
2020-06-18 18:14:22 -0400

    Disabled scrolling on details loading

--- src/components/players/details/index.js
+++ src/components/players/details/index.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import classnames from 'classnames';
+
 import Placeholder from './placeholder';
 import Card from './card';
 import QuickStats from './quickStats';
@@ -75,8 +77,10 @@ const Details = ({ player: { details, isLoading }, teams, selectedTeam: { teamId
     { title: 'pts', value: points },
   ];
 
+  console.log(isLoading);
+
   return (
-    <div onScroll={onScroll} ref={ref} className={styles.container}>
+    <div onScroll={onScroll} ref={ref} className={classnames(styles.container, isLoading && styles.scrollHidden)}>
       <Card teamColor={teamColor} player={details} playerTeamId={teamId} isSticky={isSticky} isAnimated={isAnimated} />
       <QuickStats teamColor={teamColor} quickStats={quickStats} isLoading={isLoading} />
       <TotalStats teamColor={teamColor} totalStats={totalStats} isLoading={isLoading} />
