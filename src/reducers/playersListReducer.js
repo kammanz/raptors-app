359b32066a89d2161d751b0790855b32153b4254
kermit <kermy19800@gmail.com>
2020-06-24 13:51:43 -0400

    [#63] Added reducer case reset players to playersListReducer

--- src/reducers/playersListReducer.js
+++ src/reducers/playersListReducer.js
@@ -1,5 +1,9 @@
+const initialPlayers = new Array(20).fill({});
+
 export default (state = [], action) => {
   switch (action.type) {
+    case 'RESET_PLAYERS':
+      return initialPlayers;
     case 'SET_PLAYERS':
       return action.payload;
     default:
