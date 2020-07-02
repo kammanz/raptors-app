c069128ba17dc413268ce50b54a4606c7f8018ab
kermit <kermy19800@gmail.com>
2020-07-02 17:17:24 -0400

    PR fixes

--- src/components/players/list/card.js
+++ src/components/players/list/card.js
@@ -3,37 +3,30 @@ import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import placeholderImg from 'assets/imgs/placeholder.png';
+import { formatPlayerPhotoUrl } from 'utils/stringUtils';
+
 import styles from './index.module.scss';
 
 const Card = ({
-  formatPlayerPhotoUrl,
-  getSelectedPlayer,
-  history,
   player,
-  placeholderImg,
-  firstName,
-  heightInFeet,
-  heightInInches,
-  jerseyNumber,
-  lastName,
-  personId,
-  position,
-  teamColor,
-  teamId,
-  urlName,
-  weight,
-  isSelected,
+  player: { person_id, first_name, last_name, jersey_number, position_full, height_ft, height_in, weight_lbs },
+  getSelectedPlayer,
+  selectedPlayerId,
+  selectedTeam: { teamId, teamColor, urlName },
+  history,
 }) => {
+  const isSelected = person_id === selectedPlayerId;
   return (
     <div
       onClick={() => {
-        history.push(`/${urlName}/players/${personId}`);
+        history.push(`/${urlName}/players/${person_id}`);
         getSelectedPlayer(player);
       }}
       className={classnames(styles.playerCard, isSelected && styles.selectedCard)}>
       <div className={styles.imageContainer}>
         <img
-          src={personId ? formatPlayerPhotoUrl(teamId, personId) : placeholderImg}
+          src={person_id ? formatPlayerPhotoUrl(teamId, person_id) : placeholderImg}
           alt="player headshot"
           onError={(e) => (e.target.src = placeholderImg)}
         />
@@ -42,14 +35,14 @@ const Card = ({
       <div style={{ backgroundColor: isSelected && teamColor }} className={styles.detailsContainer}>
         {!!Object.keys(player).length && (
           <>
-            <div className={styles.number}>{jerseyNumber}</div>
+            <div className={styles.number}>{jersey_number}</div>
             <div className={styles.details}>
               <div className={styles.name}>
-                {firstName} {lastName}
+                {first_name} {last_name}
               </div>
-              <div className={styles.position}>{position.replace('-', ' - ')}</div>
+              <div className={styles.position}>{position_full.replace('-', ' - ')}</div>
               <div className={styles.size}>
-                {heightInFeet}' {heightInInches}, {weight} lbs
+                {height_ft}' {height_in}, {weight_lbs} lbs
               </div>
             </div>
           </>
@@ -60,37 +53,42 @@ const Card = ({
 };
 
 Card.propTypes = {
-  formatPlayerPhotoUrl: PropTypes.func.isRequired,
+  formatPlayerPhotoUrl: PropTypes.func,
   getSelectedPlayer: PropTypes.func.isRequired,
-  history: PropTypes.object.isRequired,
+  history: PropTypes.object,
   player: PropTypes.object.isRequired,
-  placeholderImg: PropTypes.string.isRequired,
-  firstName: PropTypes.string,
-  heightInFeet: PropTypes.string,
-  heightInInches: PropTypes.string,
-  jerseyNumber: PropTypes.string,
-  lastName: PropTypes.string,
-  personId: PropTypes.string,
-  position: PropTypes.string,
+  placeholderImg: PropTypes.string,
+  first_name: PropTypes.string,
+  height_ft: PropTypes.string,
+  height_in: PropTypes.string,
+  jersey_number: PropTypes.string,
+  last_name: PropTypes.string,
+  person_id: PropTypes.string,
+  position_full: PropTypes.string,
+  selectedPlayerId: PropTypes.string,
   teamColor: PropTypes.string,
   teamId: PropTypes.string,
   urlName: PropTypes.string,
-  weight: PropTypes.string,
+  weight_lbs: PropTypes.string,
   isSelected: PropTypes.bool,
 };
 
 Card.defaultProps = {
-  firstName: null,
-  heightInFeet: null,
-  heightInInches: null,
-  jerseyNumber: null,
-  lastName: null,
-  personId: null,
-  position: null,
+  formatPlayerPhotoUrl: null,
+  history: null,
+  placeholderImg: null,
+  first_name: null,
+  height_ft: null,
+  height_in: null,
+  jersey_number: null,
+  last_name: null,
+  person_id: null,
+  position_full: null,
+  selectedPlayerId: null,
   teamColor: null,
   teamId: null,
   urlName: null,
-  weight: null,
+  weight_lbs: null,
   isSelected: false,
 };
 
