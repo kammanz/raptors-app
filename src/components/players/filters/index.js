64251ebb70677de0cb0b436cd910589a3c77ce5d
Kammanz <kammanz@Kammanzs-MacBook-Pro.local>
2020-07-16 18:34:38 -0400

    first commit

--- src/components/players/filters/index.js
+++ src/components/players/filters/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './index.module.scss';
 
 const Filters = () => {
-  return <div className={styles.filters} />;
+  return <div className={styles.filters}>time to test</div>;
 };
 
 export default Filters;
